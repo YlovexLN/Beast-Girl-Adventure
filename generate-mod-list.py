f8ed0bd360ba29bf8b3b9128d772eebb5b0c3c7e
YlovexLN <1584544837@qq.com>
2023-02-24 23:49:07 +0800

    API给的信息不全，重写

--- generate-mod-list.py
+++ generate-mod-list.py
@@ -2,10 +2,13 @@ import requests
 import json
 
 # 定义 mod ID
-mod_id = 301339
+mod_id = "357540"
 
+mod_loader = "fabric"
+
+game_version = "1.18.2"
 # 定义 cfwidget URL
-url = f'https://api.cfwidget.com/{mod_id}'
+url = f'https://api.cfwidget.com/{mod_id}?loader={mod_loader}&version={game_version}'
 
 # 发送 GET 请求并获取响应
 response = requests.get(url)
@@ -15,13 +18,13 @@ if response.status_code == 200:
     response_json = response.json()
     # 获取 mod 的各种信息
     mod_urls = response_json['urls']
-    mod_files = response_json['files']
-    mod_downloads = response_json['downloads']
+    mod_files = response_json['files'][0]
+    mod_download = response_json['download']
     # 将 mod 的信息存储到一个 Python 字典中
     mod_info = {
         'urls': mod_urls,
         'files': mod_files,
-        'downloads': mod_downloads
+        'downloads': mod_download
     }
     # 将 mod 信息写入 JSON 文件
     with open('mod-list.json', 'w') as f:
