9e3d5618bd2cec0825c94eff9344b3a3edfa878b
YlovexLN <1584544837@qq.com>
2023-02-25 13:31:27 +0800

    增加通过游戏版本对mod进行筛选

--- generate-mod-list.py
+++ generate-mod-list.py
@@ -1,19 +1,21 @@
 import requests
 import json
 
+API_KEY = '$2a$10$54xio0nrZbsEiNfBLzq35.9NBvWrapx6UecXBQLnH9sHYuIiHGwJu'
+
 headers = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'x-api-key': '$2a$10$54xio0nrZbsEiNfBLzq35.9NBvWrapx6UecXBQLnH9sHYuIiHGwJu'
+    'x-api-key': API_KEY
 }
 
-# 读取mod_ids.json文件中的mod id到一个列表中
 with open('mod-ids.json', 'r') as f:
     mod_ids = json.load(f)
 
 all_mod_info = []  # 存储所有 mod 的信息
 
 for mod_id in mod_ids:
+
     url = f'https://api.curseforge.com/v1/mods/{mod_id}'
 
     response = requests.get(url, headers=headers)
@@ -41,7 +43,6 @@ for mod_id in mod_ids:
                 'fileDate': file['fileDate'],
                 'downloadUrl': file['downloadUrl'],
                 'gameVersions': file['gameVersions']
-
             }
             mod_latest_files_info.append(file_info)
 
@@ -54,9 +55,27 @@ for mod_id in mod_ids:
             'latestFiles': mod_latest_files_info
         }
 
+        # 筛选最新文件的 gameVersions 中有 1.16.5 和 Forge 的版本
+        filtered_latest_files = []
+        for latest_file in mod_latest_files_info:
+            if '1.16.5' in latest_file['gameVersions'] and 'Forge' in latest_file['gameVersions']:
+                game_versions = latest_file['gameVersions']
+                if isinstance(game_versions, list):
+                    game_versions = ''.join(game_versions)
+                game_version_start_index = game_versions.find('{Forge')
+                if game_version_start_index != -1:
+                    game_version_end_index = game_versions.find(
+                        '}', game_version_start_index) + 1
+                    # 提取 gameVersions 中满足条件的版本段
+                    filtered_game_versions = game_versions[game_version_start_index:game_version_end_index]
+                    # 替换原来的 gameVersions
+                    latest_file['gameVersions'] = filtered_game_versions
+                filtered_latest_files.append(latest_file)
+
+        mod_info['latestFiles'] = filtered_latest_files
         all_mod_info.append(mod_info)  # 将该 mod 的信息存储到总列表中
     else:
         print(f"获取 mod {mod_id} 信息失败：{response.status_code}")
 
 with open('mod-list.json', 'w') as f:
-    json.dump(all_mod_info, f)
+    json.dump(all_mod_info, f, ensure_ascii=False, indent=4)
